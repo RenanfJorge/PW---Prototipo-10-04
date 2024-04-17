5a7353f9d2b79c82ca5efcb3e7452d3e9f5f7a96
RenanfJorge <146784713+RenanfJorge@users.noreply.github.com>
2024-04-17 07:12:17 -0300

    Add files via upload

---
+++ PW---FrameWork-main/PW---FrameWork-main/css/js/Cadastrar.js
@@ -0,0 +1,65 @@
+document.querySelector("#botao-cadastrar").addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    const form = document.querySelector("form")
+
+
+    const tarefa = {
+        titulo: form.titulo.value,
+        descricao: form.descricao.value,
+        score: form.score.value
+    }
+
+    const titulo = form.titulo.value
+    const descricao = form.descricao.value
+    const score = form.score.value
+
+    if (validar(tarefa)){
+        console.log(tarefa)
+        let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+        tarefas.push(tarefa)
+        localStorage.setItem("tarefas",JSON.stringify(tarefas))
+
+        window.location = "/epictask"
+    }
+})
+
+function validar(tarefa){
+
+    let valid = true
+
+    limparErros()
+
+    if(tarefa.titulo === ""){
+        document.querySelector("#titulo").classList.add("is-error")
+        document.querySelector("#titulo-error").innerText = "O título é obrigatório!"
+        valid = false
+    }
+
+    if(tarefa.descricao === ""){
+        document.querySelector("#descricao").classList.add("is-error")
+        document.querySelector("#descricao-error").innerText = "A descrição é obrigatória!"
+        valid = false
+    }
+
+    if(tarefa.score <= 0){
+        document.querySelector("#score").classList.add("is-error")
+        document.querySelector("#score-error").innerText = "Os pontos devem ser maior que zero!"
+        valid = false
+    }
+
+    return valid
+}
+
+function limparErros(){
+
+    document
+
+        .querySelectorAll(".nes-input.is-error, .nes-textarea.is-error")
+        .forEach(campo => campo.classList.remove("is-error"))
+
+    document
+        .querySelectorAll("span.is-error")
+        .forEach(span=>span.innerText="")
+
+}
